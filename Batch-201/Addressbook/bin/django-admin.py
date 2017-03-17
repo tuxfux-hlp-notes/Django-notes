a22da6328258cccd0e40c7ace2164d2c42eb0e1d
devopslearn <devopslearn@gmail.com>
2017-03-17 20:58:44 +0530

    removed the AddressBook virtual env

--- Batch-201/Addressbook/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/key2gyaan/Documents/git_repositories/tuxfux-hlp-notes/Django-notes/Batch-201/Addressbook/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
