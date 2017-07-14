aeff73b26fff026977c0091afe08f542f71813b9
tuxfux-hlp-notes <tuxfux-hlp-notes@gmail.com>
2017-07-14 08:26:01 +0530

    day 3

--- Batch-203/src/blog/views.py
+++ Batch-203/src/blog/views.py
@@ -6,7 +6,21 @@ from django.http import HttpResponse
 def hello_world(request):
 	return HttpResponse("Hello!! world \n")
 
+# example 1:
+# def test_hello(request):
+# 	f = open('/home/khyaathi/Documents/tuxfux-hlp-notes/Django-notes/Batch-203/src/blog/templates/test.html')
+# 	content = f.read()
+# 	return HttpResponse(content)
+
+
+# example 2
 def test_hello(request):
-	f = open('/home/khyaathi/Documents/tuxfux-hlp-notes/Django-notes/Batch-203/src/blog/templates/test.html')
-	content = f.read()
-	return HttpResponse(content)
+	context = {}
+	return render(request,'test.html',context)
+
+# example 3
+def testdata(request):
+	context = {'name1':'arjouth-a','blogname1':'arjouth first blog','message1':'Today is friday.'
+				,'name2':'deepthi-a','blogname2':'deepthi first blog','message2':'Today is friday.'
+				,'name3':'govardan-a','blogname3':'govardhan first blog','message3':'Today is friday.'}
+	return render(request,'testdata.html',context)	
--- Batch-203/src/mysite/urls.py
+++ Batch-203/src/mysite/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'blog.views.hello_world', name='home'),
     url(r'^test/', 'blog.views.test_hello', name='testhome'),
+    url(r'^testdata/', 'blog.views.testdata', name='testdata'),
 ]
