d7b9a60baed42391862cca407d46fb39e8777520
tuxfux-hlp-notes <tuxfux-hlp-notes@gmail.com>
2017-10-10 08:01:11 +0530

    Model Relations

--- Batch-204/src/check_app/admin.py
+++ Batch-204/src/check_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+# many to one relationship
+# from .models import Reporter,Article
+# admin.site.register(Reporter)
+# admin.site.register(Article)
+
+from .models import Place,Restaurant,Waiter
+admin.site.register(Place)
+admin.site.register(Restaurant)
+admin.site.register(Waiter)
--- Batch-204/src/check_app/models.py
+++ Batch-204/src/check_app/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+# one to many relationships
+
+from django.db import models
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.DO_NOTHING)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.headline
+
+    class Meta:
+        ordering = ('headline',)
+
+##
+# one to one relationship
+#
+
+
+from django.db import models
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return "%s the restaurant" % self.place.name
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return "%s the waiter at %s" % (self.name, self.restaurant)
